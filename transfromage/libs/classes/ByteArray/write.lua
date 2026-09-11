11b859f4d2a94748a4c8e9face5fa01fffca3d6f
Minstfm <22301175+Minstfm@users.noreply.github.com>
2026-09-11 21:21:04 +0000

    Remove ULEB128 not needed

--- transfromage/libs/classes/ByteArray/write.lua
+++ transfromage/libs/classes/ByteArray/write.lua
@@ -115,26 +115,6 @@ ByteArray.writeBool = function(self, bool)
 end
 
 --[[@
-	@name writeULEB128
-	@desc Inserts an unsigned variable-length integer (ULEB128) into the byte array.
-	@param value<int> A positive integer.
-	@returns byteArray Object instance.
-]]
-byteArray.writeULEB128 = function(self, value)
-	while true do
-		local byte = bit_band(value, 0x7F)
-		value = bit_rshift(value, 7)
-		if value == 0 then
-			self:write8(byte)
-			break
-		else
-			self:write8(bit_bor(byte, 0x80))
-		end
-	end
-	return self
-end
-
---[[@
 	@name writeSLEB128
 	@desc Inserts a signed variable-length integer (SLEB128) into the byte array.
 	@param value<int> A signed integer.
